cf240e55f1813d6e578050a3e2a1637a4eba7056
David Price <davidwillprice@gmail.com>
2020-08-04 18:37:02 +0100

    Added Privacy Policy page and another Reey font fix

---
+++ src/pages/privacy-policy.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Layout from '../components/layout';
+
+export default function PrivacyPolicy() {
+	return (
+		<Layout>
+			<div>
+				<h1 css="display: inline-block;
+						border-bottom: 1px solid;">Privacy Policy</h1>
+				<p>This is the privacy policy page</p>
+			</div>
+		</Layout>
+	);
+}
