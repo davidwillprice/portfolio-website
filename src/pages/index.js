5e58af444417992c06c20277285587fab9fe05bb
David Price <davidwillprice@gmail.com>
2020-09-28 21:44:55 +0100

    Home and footer improvements

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,20 @@
 import React from 'react';
-
 import Layout from '../components/layout';
+import HomeStyles from '../styles/home.module.css';
+
+import SolarImg from '../images/solargraph-test.png'
 
 export default function Home() {
 	return (
 		<Layout>
-			<div>
-				<h1>David Price</h1>
-				<h2>Web developer & designer</h2>
+			<div className={HomeStyles.homeTitleBanCon}>
+				<div className={HomeStyles.homeTitleBan}>
+					<img src={SolarImg} />
+					<div className={HomeStyles.homeTitleBanText}>
+						<h1 className={HomeStyles.homeTitle}> David Price</h1>
+						<h2 className={HomeStyles.homeSubtitle}>Web developer & designer</h2>
+					</div>
+				</div>
 			</div>
 		</Layout>
 	);
