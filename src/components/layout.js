d4a15e24339d10b427a7c8ff344d3df6c694ef42
David Price <davidwillprice@gmail.com>
2020-10-03 22:55:48 +0100

    Keyboard accessibility

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,28 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import globalStyles from '../styles/global.module.css';
 import Header from './header.js';
 import Footer from './footer.js';
 
-/*import { useStaticQuery, graphql } from 'gatsby';*/
-export default function Layout({ children }) {
-	/*const data = useStaticQuery(
-		graphql`
-			query {
-				site {
-					siteMetadata {
-						title
-					}
-				}
-			}
-		`
-	);
-	*/
-	return (
-		<div className={globalStyles.pageCon}>
-			<Header />
-			<div className={globalStyles.contentCon}>{children}</div>
-			<div className={globalStyles.test} />
-			<Footer />
-		</div>
-	);
+class Layout extends Component {
+	state = {
+		usingMouse: false
+	};
+	//Using mouse, updating state
+	usingMouseFn = () => {
+		this.setState({ usingMouse: true });
+	}
+	//Using keyboard, updating state
+	usingKeysFn = () => {
+		this.setState({ usingMouse: false });
+	}
+
+	componentDidMount() {
+		//User is using a mouse
+		document.body.addEventListener('mousedown', this.usingMouseFn);
+		//User is using keyboard
+		document.body.addEventListener('keydown', this.usingKeysFn);
+	}
+
+	render() {
+		//Update class depending on if the user last used a mouse or keyboard
+		const navType = this.state.usingMouse ? globalStyles.usingMouse : globalStyles.usingKeys;
+
+		return (
+			<div className={globalStyles.pageCon + ' ' + navType}>
+				<Header />
+				<div className={globalStyles.contentCon}>{this.props.children}</div>
+				<Footer />
+			</div>
+		);
+	}
 }
+
+export default Layout;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,20 @@
 import React from 'react';
 import Layout from '../components/layout';
 
-import HomeStyles from '../styles/home.module.css';
+import IndexStyles from '../styles/index.module.css';
 import SolarSVGStyles from '../styles/solarsvg.css';
 
 import SolarSVG from '../svgs/solar.svg';
 
-export default function Home() {
+export default function Index() {
 	return (
 		<Layout>
-			<div className={HomeStyles.homeTitleBanCon}>
-				<div className={HomeStyles.homeTitleBan}>
-					<SolarSVG className={HomeStyles.solarSVG} />
-					<div className={HomeStyles.homeTitleBanText}>
-						<h1 className={HomeStyles.homeTitle}> David Price</h1>
-						<h2 className={HomeStyles.homeSubtitle}>Web developer & designer</h2>
+			<div className={IndexStyles.indexTitleBanCon}>
+				<div className={IndexStyles.indexTitleBan}>
+					<SolarSVG className={IndexStyles.solarSVG} />
+					<div className={IndexStyles.indexTitleBanText}>
+						<h1 className={IndexStyles.indexTitle}> David Price</h1>
+						<h2 className={IndexStyles.indexSubtitle}>Web developer & designer</h2>
 					</div>
 				</div>
 			</div>
