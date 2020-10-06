6f10a3d5644ebf2ee35998189b278115497c3f7c
David Price <davidwillprice@gmail.com>
2020-10-06 19:37:16 +0100

    LYP Digital additions

--- src/pages/web/loveyourpostcode.js
+++ src/pages/web/loveyourpostcode.js
@@ -1,14 +1,37 @@
 import React from 'react';
-/*import { Link } from 'gatsby';*/
 import Layout from '../../components/layout';
-import TitleBanner from '../../components/title-banner';
+
+
+import LypDigStyles from '../../styles/lypdig.module.css';
+import LypLogo from '../../svgs/lyp-logo.svg';
 
 export default function LypDigital() {
     return (
         <Layout>
-            <TitleBanner>
-                <h1>Graphic design</h1>
-            </TitleBanner>
-        </Layout>
+            <div className={LypDigStyles.introCon}>
+                <div className={LypDigStyles.logoCon}>
+                    <LypLogo className={LypDigStyles.logo} />
+                    <h3>Digital Design</h3>
+                </div>
+                <div className={LypDigStyles.introText}>
+                    <p>I worked for West Midlands based 'Love Your Postcode Estate Agents' full-time as a Web / Graphic Designer between August 2015 and February 2019.</p>
+                    <p>Duties included:</p>
+                    <ul>
+                        <li>Using HTML, CSS and JavaScript to develop and manage the Love Your Postcode Wordpress website;</li>
+                        <li>Coding marketing emails with maximum compatibility;</li>
+                        <li>Monitoring user journeys and general website performance using Google Analytics;</li>
+                        <li>Managing Google Adword search, display, video and app campaigns;</li>
+                    </ul>
+                </div>
+            </div>
+            <ul className={LypDigStyles.introTextContin}>
+                <li>
+                    Working as part of a team to improve website SEO: Building backlinks, managing Google Search Console, keyword analysis, meta titles/descriptions, header structure and other on-page optimisation;
+            </li>
+                <li>
+                    Copywriting website pages and blog content.
+                </li>
+            </ul>
+</Layout>
     );
 }
