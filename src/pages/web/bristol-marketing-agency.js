74e9cdfe908ee8d731f0eea4a310518d2138bbd6
David Price <davidwillprice@gmail.com>
2020-10-08 19:39:11 +0100

    Bristol agency intro

--- src/pages/web/bristol-marketing-agency.js
+++ src/pages/web/bristol-marketing-agency.js
@@ -1,17 +1,18 @@
 import React from 'react';
 /*import { Link } from 'gatsby';*/
 import Layout from '../../components/layout/layout';
-import TitleBanner from '../../components/layout/title-banner';
 
 import BrisAgencyStyles from '../../styles/page/bristolagency.module.css';
 
 import BristolLandscapeSVG from '../../svgs/bristol-landscape.svg'
+import BristolAgencyLogoSVG from '../../svgs/bristol-marketing-agency-logo.svg'
 
 export default function bristolAgency() {
     return (
         <Layout>
             <div className={BrisAgencyStyles.logoCon}>
-                <BristolLandscapeSVG />
+                <BristolLandscapeSVG className={BrisAgencyStyles.bristolLandscape} />
+                <BristolAgencyLogoSVG className={BrisAgencyStyles.bristolAgencyLogo} />
             </div>
         </Layout>
     );
