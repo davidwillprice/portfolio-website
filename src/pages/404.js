0da728e31718ba6e11f35139d4d304625ac9b7bd
David Price <davidwillprice@gmail.com>
2020-10-25 18:00:32 +0000

    Added 404 text

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import Layout from '../components/layout/layout';
+import { Link } from 'gatsby';
+import TitleBanner from '../components/layout/title-banner';
+
+import MissingStyles from '../styles/page/missing-page.module.css';
 
 export default function FourZeroFour() {
 	return (
 		<Layout>
-			<div>
-				<h1 css="display: inline-block;
-						border-bottom: 1px solid;">404</h1>
+			<TitleBanner>
+				<h1>Page not found</h1>
+			</TitleBanner>
+			<div className={MissingStyles.container}>
+				<p>Sorry, I'm afraid the page you are looking for could not be found.</p>
+				<Link to={`/`}>
+					<p>Return to the homepage.</p>
+				</Link>
 			</div>
 		</Layout>
 	);
