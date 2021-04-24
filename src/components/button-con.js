025cd558412ee58870417cd208feed4720157186
David Price <davidwillprice@gmail.com>
2021-04-24 16:58:24 +0100

    Added button container

---
+++ src/components/button-con.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react"
+
+import buttonConStyles from "../styles/component/button-con.module.scss"
+
+class ButtonCon extends Component {
+  render() {
+    return (
+      <div
+        className={`${buttonConStyles.con} ${
+          this.props.dirTxtRight ? buttonConStyles.alignRight : ""
+        }`}
+      >
+        {this.props.children}
+      </div>
+    )
+  }
+}
+export default ButtonCon
