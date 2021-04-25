6496e2d30905d2095a5429f6543ba79e4637dc8f
David Price <davidwillprice@gmail.com>
2021-04-25 14:37:55 +0100

    Added smaller version of the brush mask image

--- src/components/sq-brush-mask.js
+++ src/components/sq-brush-mask.js
@@ -1,31 +1,44 @@
-import React, { Component } from 'react';
-import sqBrushStyles from '../styles/component/sqbrush.module.scss';
+import React, { Component } from "react"
+import sqBrushStyles from "../styles/component/sqbrush.module.scss"
 
-import { Link } from 'gatsby';
+import { Link } from "gatsby"
 
 class SqBrushMask extends Component {
-	render() {
-		const workExampleClasses = this.props.dirTxtRight
-			? sqBrushStyles.workExampleCon + ' ' + sqBrushStyles.brushMaskCon + ' ' + sqBrushStyles.workExampleConR
-			: sqBrushStyles.workExampleCon + ' ' + sqBrushStyles.brushMaskCon;
-		return (
-			<div className={workExampleClasses}>
-				<div className={sqBrushStyles.workExampleText}>{this.props.children}</div>
-				<Link className={sqBrushStyles.workExampleLink} aria-label={this.props.linkDesc} to={this.props.linkUrl}>
-					<div className={sqBrushStyles.brushPreviewCon}>
-						<div className={sqBrushStyles.brushPreview}>
-							<img
-								alt={this.props.imgAlt}
-								src={this.props.img}
-								className={sqBrushStyles.image}
-								style={{ maskImage: 'url(' + this.props.brush + ')', maskSize: '100%', WebkitMaskImage: 'url(' + this.props.brush + ')' }}
-							/>
-						</div>
-					</div>
-				</Link>
-			</div>
-		);
-	}
+  render() {
+    return (
+      <div
+        className={`${
+          this.props.dirTxtRight ? sqBrushStyles.workExampleConR : ""
+        } ${this.props.size ? sqBrushStyles.small : ""} ${
+          sqBrushStyles.workExampleCon
+        } ${sqBrushStyles.brushMaskCon}`}
+      >
+        <div className={sqBrushStyles.workExampleText}>
+          {this.props.children}
+        </div>
+        <Link
+          className={sqBrushStyles.workExampleLink}
+          aria-label={this.props.linkDesc}
+          to={this.props.linkUrl}
+        >
+          <div className={sqBrushStyles.brushPreviewCon}>
+            <div className={sqBrushStyles.brushPreview}>
+              <img
+                alt={this.props.imgAlt}
+                src={this.props.img}
+                className={sqBrushStyles.image}
+                style={{
+                  maskImage: "url(" + this.props.brush + ")",
+                  maskSize: "100%",
+                  WebkitMaskImage: "url(" + this.props.brush + ")",
+                }}
+              />
+            </div>
+          </div>
+        </Link>
+      </div>
+    )
+  }
 }
 
-export default SqBrushMask;
+export default SqBrushMask
