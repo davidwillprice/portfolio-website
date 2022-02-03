c2a788a2442d1b572852d790ca65c6c151ac4e12
David Price <davidwillprice@gmail.com>
2022-02-03 22:07:15 +0000

    Moved F1 prediction game Help section to its own component

--- src/components/f1/container.js
+++ src/components/f1/container.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react"
 import NavBar from "./navBar"
+import Help from "./help"
 import * as f1PredictCSS from "../../styles/component/f1-prediction-game.module.scss"
 
 //All entrant starting (driver/team) data
@@ -1421,90 +1422,7 @@ export default class F1Container extends Component {
         />
       )
     } else if (this.state.mode === "help") {
-      display = (
-        <div className={f1PredictCSS.helpCon}>
-          <h1>F1 Prediction Game&nbsp;2021</h1>
-          <h2>Rules</h2>
-          <ol>
-            <li>
-              One penalty point is awarded for every position that a driver/team
-              is from their actual standing - These are shown to the right of
-              each predicted table row;
-            </li>
-            <div className={f1PredictCSS.preview}>
-              <div className={f1PredictCSS.tableRow}>
-                <div
-                  className={`${f1PredictCSS.entrantBlock} ${f1PredictCSS.rbr}`}
-                >
-                  <div className={f1PredictCSS.position}>
-                    <span>2</span>
-                  </div>
-                  <div className={f1PredictCSS.divider}></div>
-                  <div className={f1PredictCSS.entrantName}>
-                    <span className={f1PredictCSS.fullName}>Verstappen</span>
-                  </div>
-                </div>
-                <div
-                  className={`${f1PredictCSS.posDiff} ${f1PredictCSS.diffMed}`}
-                >
-                  <span>5</span>
-                </div>
-              </div>
-              <p>
-                Verstappen was predicted to finish 2nd, but is actually 5
-                positions away from that in the current standings.
-              </p>
-            </div>
-            <li>
-              The total penalty points for each player is shown at the bottom of
-              their prediction tables;
-            </li>
-            <li>
-              Each players' accuracy rating in the leaderboard shows how close
-              they are to a prefectly predicted table;
-            </li>
-            <li>
-              The player(s) with the most accurate driver and constructor tables
-              at the end of the season win.
-            </li>
-          </ol>
-          <h2>Additional Info</h2>
-          <ul>
-            <li>
-              If players have equal accuracy ratings, the player with the more
-              perfect predictions will be ranked higher. If they are still even,
-              the player who was only 1 position off with more of their
-              predictions will be ranked higher. If they are still even, the
-              player who was only 2 positions off with more of their predictions
-              will be ranked higher and so on.
-            </li>
-            <li>
-              The 'Average' prediction tables were worked out by calculating the
-              mean position of each driver/team across the other player's
-              prediction tables;
-            </li>
-            <li>
-              Stand-in drivers won't be added to the driver standings
-              (Hulkenberg didn't join the game's driver standings last year);
-            </li>
-            <li>
-              Other than that, the game's standings will be ordered as they are
-              in real life;
-            </li>
-            <li>
-              <a href="https://www.formula1.com/en/latest/article.f1-power-rankings-weve-ranked-the-teams-from-10-to-1-after-pre-season.6DKunC8CA9nHHu6MkuiIQQ.html">
-                formula1.com
-              </a>{" "}
-              and{" "}
-              <a href="https://twitter.com/karunchandhok/status/1371374740217159680">
-                Sky F1 Analyst Karun Chandok
-              </a>{" "}
-              made their predictions after preseason testing so had an
-              advantage.
-            </li>
-          </ul>
-        </div>
-      )
+      display = <Help year="2022" />
     } else if (this.state.mode === "stats") {
       display = (
         <div className={f1PredictCSS.statsCon}>
