713a715cd446f25648d6f2c25dedf1c34e61d820
David Price <davidwillprice@gmail.com>
2022-02-05 15:06:24 +0000

    Refactored calculation functions into their own file

--- src/components/f1/2022/container.js
+++ src/components/f1/2022/container.js
@@ -9,219 +9,46 @@ import * as f1PredictCSS from "../../../styles/component/f1-prediction-game.modu
 
 import { teams, drivers, rounds, players } from "./season-data"
 
+import {
+  generateAverageTable,
+  orderAverageTable,
+  calcData,
+  orderLeaderboards,
+  calcLeaderboardRdDiffs,
+  generateEntrantDiffTotals,
+  orderEntrantDiffTotals,
+  filteredPlayers,
+} from "../calc"
+
 const year = 2022
+console.log(players)
 
-function generateAverageTables(entrantData, entrantType) {
-  //Loop over each entrant, finding their index in each player's prediction table and totalling them in a new avgPrePos property
-  for (const entrant of Object.values(entrantData)) {
-    let predictionPosTotal = 0
-    let noOfPlayers = 0
-    for (const player of Object.values(players)) {
-      //Checking player does actually predict entrant
-      if (player[entrantType + "Table"].indexOf(entrant) !== -1) {
-        predictionPosTotal += player[entrantType + "Table"].indexOf(entrant) + 1
-        noOfPlayers++
-      }
-    }
-    entrant.avgPrePos = predictionPosTotal / noOfPlayers
-    players.average[entrantType + "Table"].push(entrant)
-  }
-}
-generateAverageTables(drivers, "driver")
-generateAverageTables(teams, "team")
-function orderAverageTables(entrantType) {
-  //Sort the players by their percentage correct, highest first
-  players.average[entrantType + "Table"].sort((a, b) =>
-    a.avgPrePos > b.avgPrePos ? 1 : -1
-  )
-}
-orderAverageTables("driver")
-orderAverageTables("team")
-//Based on the player predictions and round data, calculate the differences in entrant predictions for each player, their total diff totals and their count of each diffs
-function calcData() {
-  //Loop through each round
-  for (const [roundNo, round] of Object.entries(rounds)) {
-    //Loop through each player to generate their team and driver differences
-    for (const player of Object.values(players)) {
-      //Create blank properties ready for the player's performance in this round
-      player.season["round" + roundNo] = {
-        driver: { diffTotal: 0, diffs: [], diffCounts: [] },
-        team: { diffTotal: 0, diffs: [], diffCounts: [] },
-      }
-      for (let i = 0; i < 20; i++) {
-        player.season["round" + roundNo].driver.diffCounts.push(0)
-        player.season["round" + roundNo].team.diffCounts.push(0)
-      }
-      //Loop the player's predictions, each driver and their pos difference from that round's standings
-      function calcRoundPerformance(entrantType, player) {
-        if (player[entrantType + "Table"].length > 0) {
-          for (const [predictedPos, entrant] of Object.entries(
-            player[entrantType + "Table"]
-          )) {
-            //Find the position the player predicted that entrant would come in the standings
-            const actualPos = round.standings[entrantType].indexOf(entrant)
-            //Work out how many positions the player is off
-            const posDiff = Math.abs(predictedPos - actualPos)
-            player.season["round" + roundNo][entrantType].diffCounts[posDiff]++
-            //Add the posDiff to their total for this round
-            player.season["round" + roundNo][entrantType].diffTotal += posDiff
-            //Add the entrant and their posDiff to the Player's data
-            player.season["round" + roundNo][entrantType].diffs.push({
-              entrant: entrant,
-              posDiff,
-            })
-          }
-        }
-      }
-      calcRoundPerformance("driver", player)
-      calcRoundPerformance("team", player)
-      //Populate leaderboard for selected player within selected round
-      function calcLeaderboard(entrantType, player) {
-        const maxDiff = entrantType === "driver" ? 200 : 50
-        round.leaderboards[entrantType].push({
-          player: player,
-          //The % a table is to correct is difference of 200 and the diffTotal as a percentage of 200
-          percentCorrect: Math.round(
-            ((maxDiff -
-              player.season["round" + roundNo][entrantType].diffTotal) /
-              maxDiff) *
-              100
-          ),
-          prevRdDiff: 0,
-        })
-      }
-      calcLeaderboard("driver", player)
-      calcLeaderboard("team", player)
-    }
-  }
-}
-//
-function calcLeaderboardRdDiffs(entrantType) {
-  //Loop over the rounds to calculate each of their leaderboard round changes
-  for (const [roundNo, round] of Object.entries(rounds)) {
-    //Don't calculate the leaderboard changes of the first round
-    if (roundNo > 0) {
-      //Loop over each player in order of the looped round's leaderboard
-      for (const [currentLbPos, currentPlayerData] of Object.entries(
-        round.leaderboards[entrantType]
-      )) {
-        //Find that player's position in the leaderboard of the round previous to the looped round
-        const previousLbPos = rounds[roundNo - 1].leaderboards[
-          entrantType
-        ].findIndex(
-          entrant => entrant.player.name === currentPlayerData.player.name
-        )
-        //Attach the player's leaderboard position change from the previous round to their data for the looped round
-        currentPlayerData.prevRdDiff = previousLbPos - currentLbPos
-      }
-    }
-  }
-}
-//The leadboards have been generated, but aren't in the correct order
-function orderLeaderboards(entrantType) {
-  //Loop through each round
-  for (const [roundNo, round] of Object.entries(rounds)) {
-    round.leaderboards[entrantType].sort(function (a, b) {
-      let order
-      //If their percentage correct is the same, sort by perfect predictions
-      if (a.percentCorrect === b.percentCorrect) {
-        //Loop from 0 to 19
-        //If a has bigger diffCount than b return 1
-        for (let i = 0; i < 20; i++) {
-          if (
-            a.player.season["round" + roundNo][entrantType].diffCounts[i] <
-            b.player.season["round" + roundNo][entrantType].diffCounts[i]
-          ) {
-            order = 1
-            break
-          } else if (
-            //If b has bigger diffCount than a return -1
-            a.player.season["round" + roundNo][entrantType].diffCounts[i] >
-            b.player.season["round" + roundNo][entrantType].diffCounts[i]
-          ) {
-            order = -1
-            break
-          } else {
-            //If they have the same diffCount, continue the loop and compare a lower level of diffCount
-            continue
-          }
-        }
-      } else {
-        //Else sort by percentage correct, highest first
-        return a.percentCorrect < b.percentCorrect ? (order = 1) : (order = -1)
-      }
-      return order
-    })
-  }
-}
-calcData()
-orderLeaderboards("driver")
-orderLeaderboards("team")
-calcLeaderboardRdDiffs("driver")
-calcLeaderboardRdDiffs("team")
+//Populate players.average with prediction tables created by finding the mean positions of everyone else's predictions
+generateAverageTable(players, drivers, "driver")
+generateAverageTable(players, teams, "team")
 
-function generateEntrantDiffTotals(entrantType, entrantData) {
-  //Loop over rounds to generate the EntrantDiffTotals for each round
-  for (const roundNo of Object.keys(rounds)) {
-    //Loop over players to get their diffs for each entrant
-    for (const player of Object.values(players)) {
-      //Loop over entrants to add each entrant's diffs to each total before moving onto the next player
-      for (const entrant of Object.values(entrantData)) {
-        //Find if the entrantDiffTotals doesn't already contain an object for the entrant, push {entrant:[entrant], diffTotal:0}
-        if (
-          !rounds[roundNo].entrantDiffTotals[entrantType].find(
-            x => x.entrant === entrant
-          )
-        ) {
-          rounds[roundNo].entrantDiffTotals[entrantType].push({
-            entrant: entrant,
-            diffTotal: 0,
-          })
-        }
-        //Checking if the player made predictions for that entrantType
-        if (player.season["round" + roundNo][entrantType].diffs.length > 0) {
-          //Find entrant in player's predictions for the round
-          const entrantStanding = player.season["round" + roundNo][
-            entrantType
-          ].diffs.find(element => element.entrant.sName === entrant.sName)
-          //Find entrantDiff total
-          let entrantTotal = rounds[roundNo].entrantDiffTotals[
-            entrantType
-          ].find(element => element.entrant === entrant)
-          //Add entrantStanding.posDiff to entrant's diff total
-          entrantTotal.diffTotal += entrantStanding.posDiff
-        }
-      }
-    }
-  }
-}
-generateEntrantDiffTotals("driver", drivers)
-generateEntrantDiffTotals("team", teams)
-function orderEntrantDiffTotals(entrantType) {
-  //Loop over rounds
-  for (const round of Object.values(rounds)) {
-    //Sort the players by their percentage correct, highest first
-    round.entrantDiffTotals[entrantType].sort((a, b) =>
-      a.diffTotal > b.diffTotal ? 1 : -1
-    )
-  }
-}
-orderEntrantDiffTotals("driver")
-orderEntrantDiffTotals("team")
+//Order the prediction tables in players.average
+orderAverageTable(players, "driver")
+orderAverageTable(players, "team")
 
-//Populate filteredPlayers with the players that match the selected playerGroup and have predictions for the slected entrantType
-const filteredPlayers = function (playerGroup, entrantType) {
-  const obj = {}
-  for (const key of Object.keys(players)) {
-    if (
-      players[key].groups.includes(playerGroup) &&
-      players[key][entrantType + "Table"].length > 0
-    ) {
-      obj[key] = players[key]
-    }
-  }
-  return obj
-}
+//Based on the player predictions and round data, calculate the differences in entrant predictions for each player, their diff totals and their count of each diffs
+calcData(players, rounds)
+
+//The leadboards have been generated, but aren't in the correct order so this function will order players based on performance
+orderLeaderboards(rounds, "driver")
+orderLeaderboards(rounds, "team")
+
+//Calculate how players have moved on the leaderboards compared to the previous round
+calcLeaderboardRdDiffs(rounds, "driver")
+calcLeaderboardRdDiffs(rounds, "team")
+
+//Tally up how accurate each player has been
+generateEntrantDiffTotals(rounds, players, "driver", drivers)
+generateEntrantDiffTotals(rounds, players, "team", teams)
+
+//Order the players in each round by their percentage correct, highest first
+orderEntrantDiffTotals(rounds, "driver")
+orderEntrantDiffTotals(rounds, "team")
 
 ////Data requires from UI
 //selected entrantType
@@ -230,8 +57,8 @@ const filteredPlayers = function (playerGroup, entrantType) {
 //Selected mode
 
 ////UI requires from data
-//Number of rounds
-//filtered player data using the selected playerGroup and entrantType
+//Most/least accurately predicted driver/team of selected round and how off players were
+//Players with most/least 'controversial' driver predictions based on how much they differed from the average prediction table
 
 export default class F1Container extends Component {
   constructor(props) {
@@ -317,10 +144,6 @@ export default class F1Container extends Component {
           </div>
           {this.state.mode === "leaderboard" && (
             <Leaderboard
-              playerData={filteredPlayers(
-                this.state.playerGroup,
-                this.state.entrantType
-              )}
               entrantType={this.state.entrantType}
               roundNo={this.state.selectedRound}
               roundData={rounds}
@@ -334,6 +157,7 @@ export default class F1Container extends Component {
               playerGroup={this.state.playerGroup}
               roundNo={this.state.selectedRound}
               filteredPlayers={filteredPlayers(
+                players,
                 this.state.playerGroup,
                 this.state.entrantType
               )}
