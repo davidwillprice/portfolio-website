e411a72aaef9e74919f49d86363948d721c86e03
David Price <davidwillprice@gmail.com>
2022-02-06 14:06:58 +0000

    Moved Quicksand font from Google to self hosted, allowing me to delete the Google Fonts plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,38 +11,6 @@ module.exports = {
   plugins: [
     `gatsby-plugin-anchor-links`,
     `gatsby-plugin-sitemap`,
-    /*{
-			resolve: `gatsby-plugin-google-analytics`,
-			options: {
-				// The property ID; the tracking code won't be generated without it
-				trackingId: "UA-179325070-1",
-				// Defines where to place the tracking script - `true` in the head and `false` in the body
-				head: false,
-				// Setting this parameter is optional
-				anonymize: true,
-				// Setting this parameter is also optional
-				respectDNT: true,
-				// Avoids sending pageview hits from custom paths
-				exclude: ["/preview/**", "/do-not-track/me/too/"],
-				// Delays sending pageview hits on route update (in milliseconds)
-				pageTransitionDelay: 0,
-				// Defers execution of google analytics script after page load
-				defer: false,
-				// Any additional optional fields
-				sampleRate: 5,
-				siteSpeedSampleRate: 10,
-				cookieDomain: "davidwillprice.com",
-			}
-		},*/
-    {
-      resolve: `gatsby-plugin-google-fonts`,
-      options: {
-        fonts: [
-          `quicksand\:300,500,700`, // you can also specify font weights and styles
-        ],
-        display: "swap",
-      },
-    },
     {
       resolve: "gatsby-plugin-html-attributes",
       options: {
@@ -64,13 +32,6 @@ module.exports = {
       },
     },
     `gatsby-plugin-offline`,
-    // {
-    //   resolve: `gatsby-source-filesystem`,
-    //   options: {
-    //     name: `src`,
-    //     path: `${__dirname}/src/`,
-    //   },
-    // },
     {
       resolve: "gatsby-plugin-react-svg",
     },
